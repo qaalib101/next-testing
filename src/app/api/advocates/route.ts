f27852b7236705ee063025f035770396adab709a
Qaalib Farah <qaalib1598@gmail.com>
2025-05-02 14:52:04 -0500

    Initial commit

---
+++ src/app/api/advocates/route.ts
@@ -0,0 +1,8 @@
+import db from "../../../db";
+import { advocates } from "@/db/schema";
+
+export async function GET() {
+  const data = await db.select().from(advocates);
+
+  return Response.json({ data });
+}
